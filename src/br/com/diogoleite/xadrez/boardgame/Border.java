a569563b37d2c6703306021d683eb98228d6c4da
Diogo leite <diogo.development@gmail.com>
2019-01-30 11:22:37 -0300

    Iniciado a implementação do tabuleiro e da peça

---
+++ src/br/com/diogoleite/xadrez/boardgame/Border.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diogoleite.xadrez.boardgame;
+
+/**
+ *
+ * @author Diogo Leite
+ */
+public class Border {
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Border(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+    
+    
+    
+}
---
+++ src/br/com/diogoleite/xadrez/boardgame/Piece.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diogoleite.xadrez.boardgame;
+
+/**
+ *
+ * @author Diogo Leite
+ */
+public class Piece {
+    protected Position position;
+    private Border border;
+    public Piece(){
+    }
+    
+    public Piece(Border border){
+        this.border = border;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    protected Border getBorder() {
+        return border;
+    }
+    
+}
